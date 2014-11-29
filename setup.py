8f92b3ef9f987e1bc31dc4915073094613c99b27
Bystroushaak <bystrousak@kitakitsune.org>
2014-11-29 07:53:03 +0100

    Added __init__.py, fixed setup.py.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     license='proprietary software',
 
-    packages=find_packages("src", exclude=['ez_setup']),
+    packages=find_packages(".", exclude=['ez_setup']),
 
     include_package_data=True,
     zip_safe=True,
---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from rbottle import *

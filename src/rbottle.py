434b1194c84a00c25c0532a75cbc2a7e39e7ea92
Bystroushaak <bystrousak@kitakitsune.org>
2014-11-30 08:08:08 +0100

    Fixed bugs found by tests. Added optional parameter to json_to_params().

--- src/rbottle.py
+++ src/rbottle.py
@@ -17,31 +17,43 @@ def decode_json_body():
 
     Returns:
         obj: Structure decoded by ``json.loads()``.
+
+    Raises:
+        HTTPError: 400 in case the data was malformed.
     """
     raw_data = request.body.read()
 
-    return json.loads(raw_data)
+    try:
+        return json.loads(raw_data)
+    except ValueError as e:
+        raise HTTPError(400, e.message)
 
 
 def handle_type_error(fn):
     """
     Convert ``TypeError`` to ``bottle.HTTPError`` with ``400`` code and message
     about wrong parameters.
+
+    Raises:
+        HTTPError: 400 in case too many/too little function parameters were \
+                   given.
     """
     @wraps(fn)
     def handle_type_error_wrapper(*args, **kwargs):
         try:
             return fn(*args, **kwargs)
         except TypeError as e:
-            if "got an unexpected" in e.message:
-                raise HTTPError(400, e.message)
+            msg = e.message
+            if fn.__name__ in msg and \
+               ("got an unexpected" in msg or "takes exactly" in msg):
+                raise HTTPError(400, msg)
 
-            raise
+            raise  # This will cause 500: Internal server error
 
     return handle_type_error_wrapper
 
 
-def json_to_params(fn):
+def json_to_params(fn=None, return_json=True):
     """
     Convert JSON in the body of the request to the parameters for the wrapped
     function.
@@ -52,27 +64,41 @@ def json_to_params(fn):
     will be overwritten).
 
     If single value, add it to ``*args``.
+
+    Args:
+        return_json (bool, default True): Should the decorator automatically
+                    convert returned value to JSON?
     """
-    @wraps(fn)
-    @handle_type_error
-    def dejson_wrapper(*args, **kwargs):
-        data = decode_json_body()
+    def json_to_params_decorator(fn):
+        @wraps(fn)
+        @handle_type_error
+        def json_to_params_wrapper(*args, **kwargs):
+            data = decode_json_body()
 
-        if type(data) in [tuple, list]:
-            args += data
-        elif type(data) == dict:
-            # transport only items that are not already in kwargs
-            allowed_keys = set(data.keys()) - set(kwargs.keys())
-            for key in allowed_keys:
-                kwargs[key] = data[key]
-        elif type(data) in [bool, int, float, long, str, unicode]:
-            args += [data]
+            if type(data) in [tuple, list]:
+                args = list(args) + data
+            elif type(data) == dict:
+                # transport only items that are not already in kwargs
+                allowed_keys = set(data.keys()) - set(kwargs.keys())
+                for key in allowed_keys:
+                    kwargs[key] = data[key]
+            elif type(data) in [bool, int, float, long, str, unicode]:
+                args = list(args)
+                args.append(data)
 
-        return json.dumps(
-            fn(*args, **kwargs)
-        )
+            if not return_json:
+                return fn(*args, **kwargs)
 
-    return dejson_wrapper
+            return json.dumps(
+                fn(*args, **kwargs)
+            )
+
+        return json_to_params_wrapper
+
+    if fn:  # python decorator with optional parameters bukkake
+        return json_to_params_decorator(fn)
+
+    return json_to_params_decorator
 
 
 def json_to_data(fn):
